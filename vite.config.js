1d51d238045acc914e45842087681efa1c2ef983
Conceptura <w_blackburn@outlook.com>
2025-10-11 20:50:44 +0100

    update vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "public",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "public/index.html"),
+        areas: resolve(__dirname, "public/areas-covered.html"),
+      },
+    },
   },
   server: {
     watch: {
