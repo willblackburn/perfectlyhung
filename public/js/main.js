3a37dcbf4f5f144d42b3aa36d7531e5613933a6c
Conceptura <w_blackburn@outlook.com>
2025-10-28 16:41:26 +0000

    redo gallery / update inde and areas covered

--- public/js/main.js
+++ public/js/main.js
@@ -8,8 +8,8 @@ import Lenis from "lenis";
 const lenis = new Lenis({
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  direction: 'vertical',
-  gestureDirection: 'vertical',
+  direction: "vertical",
+  gestureDirection: "vertical",
   smooth: true,
   mouseMultiplier: 1,
   smoothTouch: false,
